a5fe9988788483c2b0481614001330b3c861045e
liamblass <126391694+liamblass@users.noreply.github.com>
2023-08-14 15:17:55 +0300

    0.0.2 - Movement Script
    Movement Script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    private Vector2 direction;
+    private Animator animator;
+
+    private void Start()
     {
+        animator = GetComponent<Animator>();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+        TakeInput();
+    }
+
+
+  private void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        SetAnimatorMovement(direction);
+    }
+
+    private void TakeInput()
+    {
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        } 
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        } 
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        } 
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+    }
+
+  private void SetAnimatorMovement(Vector2 direction)
+    {
+        animator.SetFloat("xDir", direction.x);
+        animator.SetFloat("yDir", direction.y);
+        print(animator.GetFloat("xDir"));
     }
 }
