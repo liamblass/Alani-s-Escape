3d8ba6ea1e1331699c706ba3d74f33f22f1b1c78
liamblass <126391694+liamblass@users.noreply.github.com>
2023-08-14 17:55:26 +0300

    0.0.3 - Player Animations
    diddnt finish the animations for the generic charecter

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,33 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    
     public float speed;
     private Vector2 direction;
+    private Animator animator;
 
-    void Update()
+    private void Start()
     {
-        TakeInput();
-        Move();
+        animator = GetComponent<Animator>();
+
     }
+
+    private void Update()
+    {
+
+        Move();
+        TakeInput();
+    }
+
 
     private void Move()
     {
-
+        transform.Translate(direction * speed * Time.deltaTime);
+        SetAnimatorMovement(direction);
     }
 
     private void TakeInput()
     {
+        direction = Vector2.zero;
 
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
     }
-    
 
-
-
-
-
-
+    private void SetAnimatorMovement(Vector2 direction)
+    {
+        animator.SetFloat("xDir", direction.x);
+        animator.SetFloat("yDir", direction.y);
+        print(animator.GetFloat("xDir"));
+    }
 }
+
